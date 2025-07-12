17fce54744890d45da199c7da3337a9216194b7d
oscar ochanda <oscarochanda@gmail.com>
2025-07-12 10:05:07 +0300

    The-DOM-Modifying-Elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const mainElement = document.getElementById("main");
+if (mainElement) {
+  mainElement.remove();
+}
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.textContent = "Oscar is the champion"; 
+
+document.body.append(newHeader);
